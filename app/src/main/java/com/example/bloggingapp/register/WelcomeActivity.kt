e502ac8b88f2942b4d06637cbbd81495c9ae86cb
NayanPatidar <nayanpatidar28@gmail.com>
2025-05-21 16:31:35 +0530

    Welcome Activity

--- app/src/main/java/com/example/bloggingapp/register/WelcomeActivity.kt
+++ app/src/main/java/com/example/bloggingapp/register/WelcomeActivity.kt
@@ -1,21 +1,56 @@
 package com.example.bloggingapp.register
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.bloggingapp.MainActivity
 import com.example.bloggingapp.R
+import com.example.bloggingapp.databinding.ActivityWelcomeBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private val binding:ActivityWelcomeBinding by lazy {
+        ActivityWelcomeBinding.inflate(layoutInflater)
+    }
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_welcome)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        auth = FirebaseAuth.getInstance()
+
+
+        binding.loginButton.setOnClickListener {
+            val intent = Intent(this, SignInAndRegisterActivity::class.java)
+            intent.putExtra("action", "login")
+            startActivity(intent)
+        }
+
+        binding.registerButton.setOnClickListener {
+            val intent = Intent(this, SignInAndRegisterActivity::class.java)
+            intent.putExtra("action", "register")
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
         }
     }
 }
